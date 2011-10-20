6db021180303f8e200a3eab18310d2f3120cdb4e
Antti Niiles <antti.niiles@gmail.com>
2011-10-20 18:38:33 +0300

    updated readme

--- interact.rb
+++
@@ -1,6 +0,0 @@
-IO.popen("./testapp.rb", "w+") do |pipe|
-  pipe.puts "blaah"
-  pipe.close_write
-  output = pipe.read
-  puts output
-end
--- testapp.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-puts "Execute command:"
-$command = STDIN.gets
-puts "Command executed: "+$command
